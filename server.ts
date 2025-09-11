8dbbae1cb2c2c2634ffc5d014531ba0e47b3ae6f
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-09-11 11:32:21 +0530

    Setup express server

---
+++ server.ts
@@ -0,0 +1,10 @@
+import app from './src/app.ts';
+
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+};
+
+startServer();
