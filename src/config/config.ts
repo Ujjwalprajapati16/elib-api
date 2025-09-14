e7c9340979de0843f42fc920470c6c5c0e257e71
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-09-14 17:03:14 +0530

    feat: add Cloudinary configuration and dependencies for image handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config = {
     mongo_uri: process.env.MONGO_URI as string,
     env: process.env.NODE_ENV,
     jwt_secret: process.env.JWT_SECRET as string,
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY as string,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET as string
 }
 
 export const config = Object.freeze(_config);
